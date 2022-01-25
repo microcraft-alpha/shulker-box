56af5d5b272ff0199c677fab14937c3b12a55133
tobiaxs <piotr.tobiasz.dev@gmail.com>
2022-01-25 19:23:32 +0100

    Initial commit

---
+++ shulker_box/settings.py
@@ -0,0 +1,18 @@
+"""App settings."""
+
+from environs import Env
+from pydantic import BaseSettings
+
+env = Env()
+
+
+class Settings(BaseSettings):
+    """Basic settings for the application."""
+
+    TITLE: str = "Shulker Box"
+    VERSION: str = "0.0.1"
+    DESCRIPTION: str = "API handling Minecraft items"
+    DEBUG: bool = env.bool("DEBUG", default=False)
+
+
+settings = Settings()
